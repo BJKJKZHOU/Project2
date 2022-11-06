cee9f4ae6014227a4f7c3486f05d0012fa305356
ZHOUHENG <zhouheng644@gmail.com>
2022-11-06 18:52:40 +0800

    添加项目文件。

---
+++ address_book.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>    //因为需要用到, 强制退出程序, 所以需要引入此库
+
+// 声明函数
+void toAddContacts(void);
+void toSearchNumber();
+void toContactsList();
+void toContactsData();
+void toDeleteContacts();
+void toExitSystem(void);
+
